e0b6ed8631459018a970041063e6b28ed8144a58
maansev <ma4463565@gmail.com>
2026-06-13 23:53:40 +0300

    Delete InternalBase/src directory

--- InternalBase/src/feature/misc/Misc.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-#include <Windows.h>
-
-struct CUserCmd;
-namespace Misc {
-    void Render();
-    void RunBunnyHop(CUserCmd* cmd);
-    void RunAutoAccept(HWND gameWindow);
-}
--- InternalBase/src/sdk/memory/PatternScan.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <cstdint>
-
-namespace Memory
-{
-    uintptr_t GetModuleBase(const char* module);
-    uintptr_t PatternScan(const char* module, const char* signature);
-}

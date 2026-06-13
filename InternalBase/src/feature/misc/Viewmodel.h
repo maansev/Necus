f31233cde393b7ce156e35da2b3acc4ea985a9ae
maansev <ma4463565@gmail.com>
2026-06-13 15:31:33 +0300

    Delete InternalBase/src directory

--- InternalBase/src/feature/misc/Viewmodel.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace Viewmodel {
-    // Call every frame (Present or FSN render-start). Writes the user's
-    // X/Y/Z offset and FOV into the local pawn; restores game defaults
-    // once when the feature is turned off.
-    void Update();
-}
--- InternalBase/src/feature/visuals/Visuals.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace Visuals
-{
-    void Render();
-    void RunNightMode();
-}

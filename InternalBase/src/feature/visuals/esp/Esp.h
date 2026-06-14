d1e0318d7d947259922ee8c25c800a65da12edd1
maansev <ma4463565@gmail.com>
2026-06-14 21:03:47 +0300

    Delete InternalBase/src directory

--- InternalBase/src/feature/visuals/esp/Esp.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-
-namespace ESP {
-    void Render();
-    void RenderPlayerInfo(); // Show Money: деньги + экипировка над врагами
-    void HandleModelGlow(); // ���� ����� �������
-}

b9fe73dc831b91b3fc378eba6fb308f5a78c2daa
maansev <ma4463565@gmail.com>
2026-06-15 21:17:04 +0300

    Delete InternalBase/src directory

--- InternalBase/src/feature/combat/legitbot/Aimbot.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class Aimbot {
-public:
-    static void Run(int stage);
-    static bool HasTarget();  // used by GetRawInputData hook for lock_mouse
-};
--- InternalBase/src/feature/visuals/enemycounter/EnemyCounter.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-namespace EnemyCounter {
-	void Render();
-}

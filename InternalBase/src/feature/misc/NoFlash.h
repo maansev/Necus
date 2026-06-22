6333e0a09545be99762cf527e235e5457b45ba71
maansev <ma4463565@gmail.com>
2026-06-22 14:56:49 +0300

    Delete InternalBase/src directory

--- InternalBase/src/feature/misc/NoFlash.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace NoFlash {
-    // Call once at startup to find m_flFlashMaxAlpha offset via pattern scan
-    void Init();
-    // Call every frame (from hkPresent) and on FSN stage 5
-    void Update();
-}
